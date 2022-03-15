2b97058ccd20160087154a08aaa81390d8c864ec
Thomas Mailund <mailund@birc.au.dk>
2022-03-15 15:24:17 +0100

    removed solutions

--- src/dft.py
+++ src/dft.py
@@ -11,16 +11,4 @@ def in_order(t: T | None) -> Iterable[int]:
     >>> list(in_order(tree))
     [1, 2, 3, 4, 5]
     """
-    # When we have a tree on the stack we need to traverse it,
-    # when we have an int it is a value we should emit
-    stack: list[T | int] = [t] if t else []
-    while stack:
-        v = stack.pop()
-        if isinstance(v, int):
-            yield v
-        else:
-            if v.right:
-                stack.append(v.right)
-            stack.append(v.val)
-            if v.left:
-                stack.append(v.left)
+    return  # FIXME
